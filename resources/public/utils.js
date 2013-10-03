77535b2991dce87e9ff6d599b92e0b48a1d8d552
Liam Griffiths <liamgriffiths@gmail.com>
2013-10-03 15:44:15 -0400

    moved some utility-type functions into their own place

---
+++ resources/public/utils.js
@@ -0,0 +1,11 @@
+var clearCanvas = function () {
+  canvas.width = canvas.width;
+};
+
+var randomInteger = function (i) {
+  return Math.floor(Math.random() * i);
+};
+
+var randomColor = function () {
+
+};
