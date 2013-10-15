f80adec6f5907665860e96d8953fafcc80c1f915
pyb <baccou@normalesup.org>
2013-10-15 15:41:24 -0400

    Started cljs version

--- project.clj
+++ project.clj
@@ -3,7 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "0.3.4"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1934"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-jetty-adapter "1.2.0"]
-                 [compojure "1.1.5"]])
+                 [org.clojure/tools.reader "0.7.9"] ; cljs fix
+                 [compojure "1.1.5"]]
+  :cljsbuild {
+              :builds [{
+                        ;; The path to the top-level ClojureScript source directory:
+                        :source-paths ["src-cljs"]
+                        ;; The standard ClojureScript compiler options:
+                        ;; (See the ClojureScript compiler documentation for details.)
+                        :compiler {
+                                   :output-to "resources/public/cljs.js"  ; default: target/cljsbuild-main.js
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
