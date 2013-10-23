8dd4a0bc92d595e11ca944f65e9a071b61986de6
Liam Griffiths <liamgriffiths@gmail.com>
2013-10-23 16:38:04 -0400

    use paul irishs requestAnimFrame for requestAnimationFrame fallbacks

--- resources/public/js/Utils.js
+++ resources/public/js/Utils.js
@@ -25,3 +25,14 @@ var Utils = {
   }
 
 };
+
+// borrowed from http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return window.requestAnimationFrame       ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame    ||
+         function( callback ){
+           window.setTimeout(callback, 1000 / 60);
+         };
+})();
