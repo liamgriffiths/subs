df3f6d178a226ed36c30ce402a6e47c677f2d68b
Liam Griffiths <liamgriffiths@gmail.com>
2013-11-11 11:36:34 -0500

    shared tile/item serialization

--- shared/Tile.js
+++ shared/Tile.js
@@ -8,28 +8,12 @@ function Tile(settings) {
 }
 
 Tile.prototype._out = function() {
-  var items = [];
-  for (var i = 0; i < this.items.length; i++) {
-    items.push(this.items._out());
-  }
-
   return [
     this.type,
     this.explodable,
     this.mine ? this.mine._out() : undefined,
-    items
+    this.items
   ];
-};
-
-Tile.prototype.updateFromJSON = function(json) {
-  if (json.id == this.id) {
-    this.type = json.type;
-    this.explodable = json.explodable;
-    this.items = json.items;
-    this.mine = json.mine;
-  } else {
-    throw 'wtf';
-  }
 };
 
 if (typeof module !== 'undefined') module.exports = Tile;
