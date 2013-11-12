6140fc617a07ba8704b66a1d24d2906e5a59aa80
Liam Griffiths <liamgriffiths@gmail.com>
2013-11-12 10:49:22 -0500

    removed unused collection classes

--- server/PlayersCollection.js
+++
@@ -1,9 +0,0 @@
-var PlayersCollection = require('../shared/PlayersCollection');
-var Player = require('./Player');
-
-// Update all players
-PlayersCollection.prototype.update = function() {
-};
-
-
-module.exports = PlayersCollection;
