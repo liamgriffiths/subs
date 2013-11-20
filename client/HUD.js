213f72bef031a5d007a14487110c7b3149601810
Liam Griffiths <liamgriffiths@gmail.com>
2013-11-20 14:31:14 -0500

    include players in hud

--- client/HUD.js
+++ client/HUD.js
@@ -9,12 +9,14 @@ function HUD() {
   this.power = 0;
   this.points = 0;
   this.time = 0;
+  this.players = [];
 }
 
 HUD.prototype.update = function(settings, delta) {
   this.life = settings.life;
   this.mines = settings.mines;
   this.power = settings.power;
+  this.players = settings.players;
 };
 
 HUD.prototype.draw = function() {
@@ -23,14 +25,16 @@ HUD.prototype.draw = function() {
   this.drawLife();
   this.drawMines();
   this.drawPower();
+  this.drawPlayers();
 };
 
-HUD.prototype.drawText = function(text, xpos) {
-  var ypos = 65;
+HUD.prototype.drawText = function(text, xpos, ypos) {
+  var ypos = ypos || 35;
   this.context.fillStyle = '#fff';
-  this.context.font = "bold 60px 'VT323'";
+  this.context.font = "bold 20px 'VT323'";
   this.context.fillText(text, xpos, ypos);
-}
+};
+
 HUD.prototype.drawTitle = function() {
   this.drawText("bit bomber", 40);
 };
@@ -45,5 +49,13 @@ HUD.prototype.drawMines = function() {
 
 HUD.prototype.drawPower = function() {
   this.drawText("Fire x" + this.power, 900);
+};
+
+HUD.prototype.drawPlayers = function() {
+  this.drawText("Players Connected:", 40, 70);
+
+  for (var i = 1; i < this.players.length; i++) {
+    this.drawText(this.players[i].name, 140 + i * 100, 70);
+  }
 };
 
